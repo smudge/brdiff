f0b9ab28b3f47cac77f4ea2c6fcf873b2db884da
smudge <nathan@ngriffith.com>
2020-04-20 00:16:30 -0400

    IT WORKS!!!
    Currently testing, but it seems to output
    identical images to the cpp implementation!

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,53 @@
+extern crate image;
+
+use image::{ImageBuffer, Rgb};
+
 pub struct Image {
     path: String,
+    image: ImageBuffer<Rgb<u8>, Vec<u8>>,
 }
 
 impl Image {
-    pub fn new(path: String) -> Image {
-        Image { path }
+    pub fn from_path(path: String) -> Result<Image, image::ImageError> {
+        let image = image::open(path.clone())?.into_rgb();
+
+        Ok(Image { path, image })
+    }
+
+    pub fn subtract(&self, sub_image: &Image) -> Result<Image, &str> {
+        if self.image.dimensions() != sub_image.image.dimensions() {
+            return Err("Image dimensions do not match!");
+        }
+
+        let mut output = Image {
+            image: self.image.clone(),
+            path: self.path.clone(),
+        };
+
+        for (x, y, pixel) in output.image.enumerate_pixels_mut() {
+            let subt_pixel = sub_image.image.get_pixel(x, y);
+            let r = pixel[0] as f64 - subt_pixel[0] as f64;
+            let g = pixel[1] as f64 - subt_pixel[1] as f64;
+            let b = pixel[2] as f64 - subt_pixel[2] as f64;
+
+            let luminance = 0.30 * r + 0.59 * g + 0.11 * b;
+
+            if luminance >= 0.0 {
+                *pixel = Rgb([0, 0, luminance as u8]);
+            } else {
+                let luminance = (-luminance) as u8;
+                *pixel = Rgb([luminance, 0, 0]);
+            }
+        }
+
+        Ok(output)
+    }
+
+    pub fn set_path(&mut self, path: String) {
+        self.path = path;
+    }
+
+    pub fn save(&self) -> image::ImageResult<()> {
+        self.image.save(self.path.clone())
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use brdiff::Image;
+use std::process::exit;
 
 fn show_usage(program: String) {
     eprintln!(
@@ -10,12 +11,28 @@ fn show_usage(program: String) {
 
 fn main() {
     let args: Vec<String> = std::env::args().collect();
-    if args.len() != 3 {
+    if args.len() != 4 {
         show_usage(args[0].clone());
+        exit(1);
     }
 
-    let base_image = Image::new(args[1].clone());
-    let subtraction_image = Image::new(args[2].clone());
+    let base_image = match Image::from_path(args[1].clone()) {
+        Ok(image) => image,
+        Err(e) => panic!("Error opening {}:\n{}", args[1], e),
+    };
+    let subtraction_image = match Image::from_path(args[2].clone()) {
+        Ok(image) => image,
+        Err(e) => panic!("Error opening {}:\n{}", args[2], e),
+    };
 
-    println!("Hello, world!");
+    let mut output_image = match base_image.subtract(&subtraction_image) {
+        Ok(output_image) => output_image,
+        Err(e) => panic!("Error subtracting images:\n{}", e),
+    };
+
+    output_image.set_path(args[3].clone());
+    match output_image.save() {
+        Ok(_) => (),
+        Err(e) => eprintln!("Error writing {}:\n{}", args[3], e),
+    }
 }
