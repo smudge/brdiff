b5362209ab4ade02fcc8ee457194789aa77a2d71
smudge <nathan@ngriffith.com>
2020-04-20 00:17:40 -0400

    Warn if pixel valeus are not the same across all 3 rgb channels

--- src/lib.rs
+++ src/lib.rs
@@ -19,12 +19,16 @@ impl Image {
             return Err("Image dimensions do not match!");
         }
 
+        let mut warn_about_channels = false;
         let mut output = Image {
             image: self.image.clone(),
             path: self.path.clone(),
         };
 
         for (x, y, pixel) in output.image.enumerate_pixels_mut() {
+            if pixel[0] != pixel[1] && pixel[1] != pixel[2] {
+                warn_about_channels = true;
+            }
             let subt_pixel = sub_image.image.get_pixel(x, y);
             let r = pixel[0] as f64 - subt_pixel[0] as f64;
             let g = pixel[1] as f64 - subt_pixel[1] as f64;
@@ -40,6 +44,9 @@ impl Image {
             }
         }
 
+        if warn_about_channels {
+            eprintln!("WARNING: Image has pixel values that are not the same across RGB color channels!\n{}", self.path);
+        }
         Ok(output)
     }
 
